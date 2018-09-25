4c4697f9c748210e6c095f1a8ffd0ad5be68517d
ShabeenaIqbal <shabeenaiqbal@gmail.com>
2018-09-25 23:57:18 +0500

    My first git commit of Java practice tutorial

---
+++ src/Application.java
@@ -0,0 +1,38 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		// Hello World program
+		//System.out.println("Hello World");
+		
+		//Variables
+		int Mynumber = 13;
+		byte mybyte = 127;
+		float myfloat = 3.4f;
+		double mydouble = 3.4555;
+		short myshort = 23;
+		long mylong = 23456;
+		char mychar = 't';
+		boolean myboolean = true;
+		
+		System.out.println(Mynumber);
+		System.out.println(mybyte);
+		System.out.println(myfloat);
+		System.out.println(mydouble);
+		System.out.println(myshort);
+		System.out.println(mylong);
+		System.out.println(mychar);
+		System.out.println(myboolean);
+			
+		String text = "hello";
+			
+		System.out.println("My String is:" + text);
+		
+		boolean loop = 6<1;
+		System.out.println(loop);
+		
+				
+	}
+
+}
